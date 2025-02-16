ba57293d51345ae328e3c6eff3106a9ee9102e23
SollyMollyDolly <sollymollydolly@gmail.com>
2025-02-16 14:12:48 -0300

    Hi

--- screencapture.sh
+++ screencapture.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 
-ffmpeg -video_size 1366x768 -framerate 25 -f x11grab -i :0.0 -vframes 1 /tmp/screenshot.png
-xclip -selection clipboard -t image/png -i < /tmp/screenshot.png
-rm /tmp/screenshot.png
+FILE=$(date +"Screenshot_%Y%m%d_%H%M%S.png")
+ffmpeg -video_size 1366x768 -framerate 1 -f x11grab -i :0.0 -vframes 1 /tmp/$FILE
+xclip -selection clipboard -t image/png -i < /tmp/$FILE
+rm /tmp/$FILE
